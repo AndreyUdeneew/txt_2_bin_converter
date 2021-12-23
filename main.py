755034404dad67695b7595b05b22725d99b450f4
Andrey <andrey.udeneew@mail.ru>
2021-12-23 20:30:13 +0300

    v0.3 Can convert 0xaa,0xbb to aabb

--- main.py
+++ main.py
@@ -2,43 +2,44 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import struct
 from tkinter import *
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from tkinter import filedialog
 from tkinter.filedialog import *
 import os
-import numpy as np
-from os import path
-
 
 def converter():
+    # value=[]
     input_filename = filedialog.askopenfilename(parent=window)
     fileDir = os.path.split(input_filename)[0]
-    print("wtf")
     print(fileDir)
-    print("wtf")
-    # filename = "C:/Users/Stasy/Desktop/test.txt"
-    # input_filename=fileDir+'/output2FLASH_bin.txt'
-    output_filename = fileDir + '/output2FLASH_bin.txt'
+    print(input_filename)
+    output_filename = fileDir + '/output2FLASH_bin.bin'
     # read file as string:
     f = open(input_filename, 'r')
     mytxt = f.read()
-    print("wtf")
-    print("\r\n")
-
-    # change text into binary mode:
-    binarytxt = int(mytxt,16)
-    hex_txt = hex(binarytxt)
-    # print(hex_txt)
-    # print()
+    f.close()
     # print(mytxt)
-    # binarytxt = str.encode(txt)
-    # save the bytes object
-    with open(output_filename, 'wb') as fbinary:
-        fbinary.write(hex_txt)
+    values = mytxt.split(",")
+    # for word in mytxt.split(' '):
+    #     values = str(values)
+    #     values = re.sub(r'\n', '', values)
+    #     values = re.sub(r'\]', '', values)
+    #     values = re.sub(r'\[', '', values)
+    #     values = re.sub(r'\[', '', values)
+    int_txt = []
 
+    for i in range(len(values)):
+        int_txt.append(int((values[i]), base=16))
+
+    with open(output_filename, 'wb') as fbinary:
+        # pickle.dump(int_txt, fbinary)
+        for i in range(len(values)):
+            fbinary.write(int.to_bytes(int_txt[i],1,byteorder='big'))
+        fbinary.close()
+    text0.insert(INSERT, 'Готово')
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -50,9 +51,6 @@ if __name__ == '__main__':
     window.geometry('300x100')
     window.title("txt_2_bin_converter")
 
-    # lbl0 = Label(window, text="Выбор полноэкранных картинок")
-    # lbl0.grid(column=0, row=1)
-
     text0 = Text(width=7, height=1)
     text0.grid(column=1, row=1, sticky=(W))
     # text0.pack()
@@ -61,6 +59,4 @@ if __name__ == '__main__':
     btn0.grid(column=0, row=1)
 
     window.mainloop()
-    print_hi('byeCharm')
-
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
